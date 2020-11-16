0ee981ad998aea292e354223cd3b2e68f1092b0d
Stephanie <stephanie.furlan94@gmail.com>
2020-11-16 18:18:07 +0100

    clear input, indexOf

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var surnameList = ["Imlach", "Dinsey","Newhouse", "Beaten", "Dudny", "Butteris", "Richfield", "Gooly", "Trelevan", "Bauser", "Empson","Belsher", "Potteril", "Demke", "Shirley", "Crean", "Tranfield", "Fishpond", "Polycote", "Splevin", "Inworth", "McQuilkin", "Sheekey", "Sharland", "Braycotton", "Marsham", "Stambridge", "Simek", "Salterne", "Cornuau", "Lockton", "Yurkin", "Brenneke", "Costelloe", "O'Lenechan", "Ringham", "Casaccio"];
 
 
+// alphabetic sort
 function customSort(list){
    list.sort(function (a, b) {
        if ( a.toLowerCase() < b.toLowerCase() ) {
@@ -13,35 +14,27 @@ function customSort(list){
    } )
 }
 
-// display
+function displayList(list) {
+   document.getElementById("surname-list__body").innerHTML = "";
+   var index = 0;
+   for (var i = 0; i < list.length; i++) {
+      var dataIndex = document.createElement('li');
+      var indexItem = document.createTextNode(i + 1);
+      dataIndex.appendChild(indexItem);
+      document.getElementById("surname-list__body").appendChild(dataIndex);
 
-customSort(surnameList);
-
-var index = 0;
-document.getElementById("surname-list__body").innerHTML = "";
-
-for (var i = 0; i < surnameList.length; i++) {
-
-
-   var dataIndex = document.createElement('li');
-   var indexItem = document.createTextNode(i + 1);
-   dataIndex.appendChild(indexItem);
-   document.getElementById("surname-list__body").appendChild(dataIndex);
-
-   var dataSurname = document.createElement('li');
-   var surnameItem = document.createTextNode(surnameList[i]);
-   dataSurname.appendChild(surnameItem);
-   document.getElementById("surname-list__body").appendChild(dataSurname);
-
-
-
-
-
-
-
+      var dataSurname = document.createElement('li');
+      var surnameItem = document.createTextNode(list[i]);
+      dataSurname.appendChild(surnameItem);
+      document.getElementById("surname-list__body").appendChild(dataSurname);
+   }
 }
 
+// first sort array and display;
+customSort(surnameList);
+displayList(surnameList);
 
+// on click add surname
 var add = document.getElementById("add-surname");
 add.addEventListener("click", function() {
 
@@ -54,31 +47,11 @@ add.addEventListener("click", function() {
       // add surname
       surnameList.push(surname);
       // sort surname list
-
       customSort(surnameList);
-
-      var index = 0;
-      document.getElementById("surname-list__body").innerHTML = "";
-
-      for (var i = 0; i < surnameList.length; i++) {
-
-         var dataSurname = document.createElement('li');
-         var dataIndex = document.createElement('li');
-
-
-         var surnameItem = document.createTextNode(surnameList[i]);
-         var indexItem = document.createTextNode(i + 1);
-
-         dataSurname.appendChild(surnameItem);
-         dataIndex.appendChild(indexItem);
-
-
-         if (surnameList[i] == surname) {
-            document.getElementById("add-container").innerHTML = "Your surname is at " + (i+1) + " position in the list.";
-         }
-         document.getElementById("surname-list__body").appendChild(dataIndex);
-         document.getElementById("surname-list__body").appendChild(dataSurname);
-      }
-
+      // display ordered surname list
+      displayList(surnameList);
+      // display position
+      var i = surnameList.indexOf(surname);
+      document.getElementById("add-container").innerHTML = "Your surname is at " + (i+1) + " position in the list.";
    }
 })
